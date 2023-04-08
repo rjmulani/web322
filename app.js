9675b6ad0b11874b33e8715e11c0873ed190c169
rjmulani <Rutvikmulani12340@gmail.com>
2023-04-08 14:43:36 +0500

    assignment 3 completed

--- app.js
+++ app.js
@@ -1,30 +1,143 @@
-const express = require('express');
-const expressHandlebars = require('express-handlebars');
-const indexRouter = require('./routes/index');
-const path = require('path')
-const session = require('express-session');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const indexRouter = require("./routes/index");
+const path = require("path");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-app.use(express.static(path.join(__dirname, 'images')))
+const mime = require("mime");
+const handlebars = require("handlebars");
+
+handlebars.registerHelper("eq", function (a, b) {
+  return a === b;
+});
+handlebars.registerHelper("json", function (context) {
+  return JSON.stringify(context);
+});
+app.use(express.static(path.join(__dirname, "images")));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "mySecretKey",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.engine(
+  "handlebars",
+  expressHandlebars.engine({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
 
-app.use(session({
-  secret: 'mySecretKey', // replace with your own secret key
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // set to true if using HTTPS
-}));
+app.use(express.static("public"));
 
-app.engine('handlebars', expressHandlebars.engine({
-  defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+const uri =
+  "mongodb+srv://rutvik:rutvik123@cluster0.aazvz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function createCollection() {
+  try {
+    await client.connect();
 
-app.use(express.static('public'));
+    const galleryCollection = client.db("dbs311").collection("Gallery");
 
-app.use('/', indexRouter);
+    const galleryItems = [
+      {
+        FILENAME: "../images/BathroomStuff.jpg",
+        DESCRIPTION: "Bathroom Stuff",
+        PRICE: 2000,
+        STATUS: "A",
+      },
+      {
+        FILENAME: "../images/Cereal.jpg",
+        DESCRIPTION: "Cereal",
+        PRICE: 2500,
+        STATUS: "A",
+      },
+      {
+        FILENAME: "../images/Cones.jpg",
+        DESCRIPTION: "Cones",
+        PRICE: 3200,
+        STATUS: "A",
+      },
+      {
+        FILENAME: "../images/ConesAndGrass.jpg",
+        DESCRIPTION: "Cones and grass",
+        PRICE: 6700,
+        STATUS: "A",
+      },
+      {
+        FILENAME: "../images/Leafs.jpg",
+        DESCRIPTION: "Leafs",
+        PRICE: 6700,
+        STATUS: "A",
+      },
+      {
+        FILENAME: "../images/leafs2.jpg",
+        DESCRIPTION: "More Leafs",
+        PRICE: 6700,
+        STATUS: "A",
+      },
+      {
+        FILENAME: "../images/Matches.jpg",
+        DESCRIPTION: "Matches here",
+        PRICE: 6700,
+        STATUS: "A",
+      },
+      {
+        FILENAME: "../images/Nuts.jpg",
+        DESCRIPTION: "Nuts",
+        PRICE: 6700,
+        STATUS: "A",
+      },
+      {
+        FILENAME: "../images/Peppers.jpg",
+        DESCRIPTION: "Huhhh Peppers",
+        PRICE: 6700,
+        STATUS: "A",
+      },
+      {
+        FILENAME: "../images/Rocks.jpg",
+        DESCRIPTION: "Rocks",
+        PRICE: 6700,
+        STATUS: "A",
+      },
+    ];
 
+    for (let item of galleryItems) {
+      const existingItem = await galleryCollection.findOne({
+        FILENAME: item.FILENAME,
+      });
+      if (!existingItem) {
+        await galleryCollection.insertOne(item);
+        console.log(`Inserted gallery item with filename ${item.FILENAME}`);
+      } else {
+        console.log(
+          `Gallery item with filename ${item.FILENAME} already exists`
+        );
+      }
+    }
+
+    console.log("Gallery collection created successfully");
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+
+createCollection().catch(console.dir);
+// Express app setup
+app.use("/", indexRouter);
 app.listen(3000, function () {
-  console.log('Express app listening on port 3000!');
+  console.log("Express app listening on port 3000!");
 });
--- routes/index.js
+++ routes/index.js
@@ -1,89 +1,123 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { fetchFile } = require('../services/helpers');
-const users = require('../user.json');
-const bcrypt = require('bcrypt');
+const { fetchFile } = require("../services/helpers");
+const users = require("../user.json");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const fs = require('fs');
+const fs = require("fs");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-let temp = {}
+const uri =
+  "mongodb+srv://rutvik:rutvik123@cluster0.aazvz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+let temp = {};
 
-router.get('/', function (req, res) {
-  res.render('login');
+router.get("/", function (req, res) {
+  res.render("login");
 });
 
-router.get('/login', (req, res) => {
-  res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   if (users[email]) {
-    // bcrypt.compare(password, users[email], (err, result) => {
-      if (users[email] && users[email] === password) {
-        req.session.user = email;
-        res.redirect('/gallary');
-      } else {
-        res.render('login', { message: 'Invalid email or password.' });
-      }
-    // });
+    if (users[email] && users[email] === password) {
+      req.session.user = email;
+      const galleryCollection = client.db("dbs311").collection("Gallery");
+      await galleryCollection.updateMany({}, { $set: { STATUS: "a" } });
+      res.redirect("/gallary");
+    } else {
+      res.render("login", { message: "Invalid email or password." });
+    }
   } else {
-    res.render('login', { message: 'Invalid email or password.' });
+    res.render("login", { message: "Invalid email or password." });
   }
 });
-router.get('/register', (req, res) => {
-  res.render('register');
+router.get("/register", (req, res) => {
+  res.render("register");
 });
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-
-  // Check if email and password are valid
   if (!email || !password || !confirmPassword) {
-    return res.render('register', { errorMessage: 'Please enter a valid email and password.' });
+    return res.render("register", {
+      errorMessage: "Please enter a valid email and password.",
+    });
   } else if (password !== confirmPassword) {
-    return res.render('register', { errorMessage: 'Passwords do not match.' });
+    return res.render("register", { errorMessage: "Passwords do not match." });
   }
-
-  // Check if user already exists
   if (users[email]) {
-    return res.render('register', { errorMessage: 'Email already exists.' });
+    return res.render("register", { errorMessage: "Email already exists." });
   }
-
-  // Save user to user.json
   users[email] = password;
-  fs.writeFile('./user.json', JSON.stringify(users, null, 2), (err) => {
+  fs.writeFile("./user.json", JSON.stringify(users, null, 2), (err) => {
     if (err) {
       console.error(err);
-      return res.render('register', { errorMessage: 'Error saving user.' });
+      return res.render("register", { errorMessage: "Error saving user." });
     }
     console.log(`User ${email} saved to user.json`);
     req.session.user = email;
-    res.redirect('/gallary');
+    res.redirect("/gallary");
   });
 });
-router.get('/gallary', (req, res) => {
-  if (req.session.user) {
-    const email = req.session.user;
-    fetchFile()
-      .then(({ namesWithExtension, namesWithoutExtension }) => (
-        res.render("gallary", {
-          title: "My Gallery",
-          images: [
-            { src: "/images/image1.jpg", alt: "Image 1" },
-            { src: "/images/image2.jpg", alt: "Image 2" },
-            { src: "/images/image3.jpg", alt: "Image 3" }
-          ],
-          fileNames: namesWithExtension,
-          namesWithoutFileExtension: namesWithoutExtension,
-          email: email // pass the email of the logged-in user to the gallery view
-        })
-      ))
-      .catch(err => console.error(err));
-  } else {
-    res.redirect('/login');
+router.get("/gallary", async (req, res) => {
+  try {
+    const galleryCollection = client.db("dbs311").collection("Gallery");
+    const galleryItems = await galleryCollection.find({}).toArray();
+    res.render("gallary", { email: req.session.user, galleryItems });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).send("Error retrieving gallery items");
+  }
+});
+
+router.put("/products/:productId", async (req, res) => {
+  console.log("buy end point hitted");
+  try {
+    const id = new ObjectId(req.params.productId);
+    const newStatus = req.body.status;
+
+    const galleryCollection = client.db("dbs311").collection("Gallery");
+    const result = await galleryCollection.updateOne(
+      { _id: id },
+      { $set: { STATUS: newStatus } }
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating product status." });
+  }
+});
+router.get("/order/:id", async (req, res) => {
+  try {
+    const galleryCollection = client.db("dbs311").collection("Gallery");
+    const id = new ObjectId(req.params.id);
+
+    const product = await galleryCollection.findOne({ _id: id });
+    if (!product) {
+      res.status(404).send("Product not found");
+      return;
+    }
+    res.render("orderPurchase", {
+      productId: product._id.toString(),
+      imageSrc: product.FILENAME,
+      imageName: product.DESCRIPTION,
+      imageDescription: product.DESCRIPTION,
+      imagePrice: product.PRICE,
+    });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).send("Error retrieving product information");
   }
 });
 
